64fb597049e4b071164fb636fe80581f3b7d573b
Ding He <derrick.he@gmail.com>
2024-04-02 18:09:28 +0200

    fix: code refact

--- oath2.py
+++ oath2.py
@@ -30,6 +30,23 @@ else:
     # If token exists in session state, show the token
     token = st.session_state['token']
     st.json(token)
+    
+    # Use the token to get Jira issues
+    PROJECT_KEY = "DCCP"
+    HEADERS = {'Authorization': f'Bearer {token}',
+            'Accept': 'application/json'}
+    jira_api_server = 'https://api.atlassian.com/oauth/token/accessible-resources'
+    response = requests.request("GET", f'https://{jira_api_server}/rest/api/2/search?jql=project={PROJECT_KEY}', headers=HEADERS)
+
+    # Get the JSON response body
+    if response.status_code == 200:
+        issues = response.json()
+        # Print each issue key and summary
+        for issue in issues['issues']:
+            print(f"{issue['key']}: {issue['fields']['summary']}")
+        else:
+            print(f"Error: {response.status_code} - {response.text}")
+
     if st.button("Refresh Token"):
         # If refresh token button is clicked, refresh the token
         token = oauth2.refresh_token(token)
@@ -39,21 +56,6 @@ else:
 # For local testing
 #print(st.session_state)
 
-# Use the token to get Jira issues
-PROJECT_KEY = "DCCP"
-HEADERS = {'Authorization': f'Bearer {token}',
-           'Accept': 'application/json'}
-jira_api_server = 'https://api.atlassian.com/oauth/token/accessible-resources'
-response = requests.get(f'https://{jira_api_server}/rest/api/2/search?jql=project={PROJECT_KEY}', headers=HEADERS)
-
-# Get the JSON response body
-if response.status_code == 200:
-   issues = response.json()
-   # Print each issue key and summary
-   for issue in issues['issues']:
-      print(f"{issue['key']}: {issue['fields']['summary']}")
-else:
-    print(f"Error: {response.status_code} - {response.text}")
 
 # """
 # [Login](https://auth.atlassian.com/authorize?audience=api.atlassian.com&client_id=ncXG3wFdYgzhibpX64G7nM9xhh9DxSMS&scope=read%3Ajira-work&redirect_uri=http%3A%2F%2Flocalhost%3A8502%2F&state=${YOUR_USER_BOUND_VALUE}&response_type=code&prompt=consent)
