83ef28a4964079236d6ce32be0a7c462d4ddbd04
Ding He <derrick.he@gmail.com>
2024-04-02 16:22:55 +0200

    update: requirements fix: using st secret add: .gitignore

--- oath2.py
+++ oath2.py
@@ -1,53 +1,81 @@
 import streamlit as st
-import requests, webbrowser
-from oauthlib.oauth2 import BackendApplicationClient
-from requests_oauthlib import OAuth2Session
+import requests, webbrowser, os
+from dotenv import load_dotenv
 
-st.write('Hello!')
-""" # Your client credentials
-client_id = 'client_id'
-client_secret = 'client_secret'
+load_dotenv()  # take environment variables from .env.
 
-# Create a session
-client = BackendApplicationClient(client_id=client_id)
-oauth = OAuth2Session(client=client)
 
-# Get token
-token = oauth.fetch_token(token_url='https://your-jira-server.atlassian.net/oauth/token', client_id=client_id, client_secret=client_secret)
+from streamlit_oauth import OAuth2Component
 
-# Use the token
-jira_server = 'jira_server'
-response = oauth.get(f'https://{jira_server}/rest/api/2/search?jql=project=PROJECT_KEY')
+AUTHORIZATION_URL = "https://auth.atlassian.com/authorize"
+TOKEN_URL = "https://auth.atlassian.com/oauth/token"
+REVOKE_URL = ""
+CLIENT_ID = st.secrets["CLIENT_ID"]
+CLIENT_SECRET = st.secrets["SECRET"]
+REDIRECT_URI = "https://jira-issue-tracker-mcuw6k6su7gzqyn5hnyex3.streamlit.app/"
+# For local testing
+#CLIENT_ID = os.getenv("CLIENT_ID")
+#CLIENT_SECRET = os.getenv("SECRET")
+#REDIRECT_URI = "http://localhost:8502"
+SCOPE = "read:jira-work"
 
-# Get the JSON response body
-issues = response.json()
+oauth2 = OAuth2Component(CLIENT_ID, CLIENT_SECRET, AUTHORIZATION_URL, TOKEN_URL, TOKEN_URL, REVOKE_URL)
 
-# Print each issue key and summary
-for issue in issues['issues']:
-    print(f"{issue['key']}: {issue['fields']['summary']}")
- """
+# Check if token exists in session state
+if 'token' not in st.session_state:
+  result = oauth2.authorize_button("Continue with Atlassian", REDIRECT_URI, SCOPE)
+  if result:
+    st.session_state.token = result.get('token')
+    st.rerun()
 
-""" url = 'https://auth.atlassian.com/authorize?audience=api.atlassian.com&client_id=ncXG3wFdYgzhibpX64G7nM9xhh9DxSMS&scope=read%3Ajira-work&redirect_uri=https%3A%2F%2Fjira-issue-tracker-mcuw6k6su7gzqyn5hnyex3.streamlit.app%2F&state=${YOUR_USER_BOUND_VALUE}&response_type=code&prompt=consent'
-webbrowser.open_new(url)
-headers = {'Content-Type': 'application/json'}
-response = requests.get(url, headers=headers)
-print(response.json()) """
+# For local testing
+#print(st.session_state)
 
-""" # Define the URL and the headers
-url = 'https://auth.atlassian.com/oauth/token'
-headers = {'Content-Type': 'application/json'}
+# # Your client credentials
+# client_id = 'client_id'
+# client_secret = 'client_secret'
 
-# Define the data
-data = {
-    "grant_type": "authorization_code",
-    "client_id": "YOUR_CLIENT_ID",
-    "client_secret": "YOUR_CLIENT_SECRET",
-    "code": "YOUR_AUTHORIZATION_CODE",
-    "redirect_uri": "https://YOUR_APP_CALLBACK_URL"
-}
+# # Create a session
+# client = BackendApplicationClient(client_id=client_id)
+# oauth = OAuth2Session(client=client)
 
-# Make the POST request
-response = requests.post(url, headers=headers, data=json.dumps(data))
+# # Get token
+# token = oauth.fetch_token(token_url='https://your-jira-server.atlassian.net/oauth/token', client_id=client_id, client_secret=client_secret)
 
-# Print the response
-print(response.json()) """
+# # Use the token
+# jira_server = 'jira_server'
+# response = oauth.get(f'https://{jira_server}/rest/api/2/search?jql=project=PROJECT_KEY')
+
+# # Get the JSON response body
+# issues = response.json()
+
+# # Print each issue key and summary
+# for issue in issues['issues']:
+#     print(f"{issue['key']}: {issue['fields']['summary']}")
+ 
+# """
+# [Login](https://auth.atlassian.com/authorize?audience=api.atlassian.com&client_id=ncXG3wFdYgzhibpX64G7nM9xhh9DxSMS&scope=read%3Ajira-work&redirect_uri=http%3A%2F%2Flocalhost%3A8502%2F&state=${YOUR_USER_BOUND_VALUE}&response_type=code&prompt=consent)
+# url = 'https://auth.atlassian.com/authorize?audience=api.atlassian.com&client_id=ncXG3wFdYgzhibpX64G7nM9xhh9DxSMS&scope=read%3Ajira-work&redirect_uri=https%3A%2F%2Fjira-issue-tracker-mcuw6k6su7gzqyn5hnyex3.streamlit.app%2F&state=${YOUR_USER_BOUND_VALUE}&response_type=code&prompt=consent'
+# webbrowser.open_new(url)
+# headers = {'Content-Type': 'application/json'}
+# response = requests.get(url, headers=headers)
+# print(response.json())
+# """
+# Define the URL and the headers
+# url = 'https://auth.atlassian.com/oauth/token'
+# headers = {'Content-Type': 'application/json'}
+
+# # Define the data
+# data = {
+#     "grant_type": "authorization_code",
+#     "client_id": "YOUR_CLIENT_ID",
+#     "client_secret": "YOUR_CLIENT_SECRET",
+#     "code": "YOUR_AUTHORIZATION_CODE",
+#     "redirect_uri": "https://YOUR_APP_CALLBACK_URL"
+# }
+
+# # Make the POST request
+# response = requests.post(url, headers=headers, data=json.dumps(data))
+
+# # Print the response
+# print(response.json())
